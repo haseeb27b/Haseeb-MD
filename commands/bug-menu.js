54cd9e9a5b6cd83b2935e1f76f7a0129af91ea72
haseeb27b <haseeb27b@gmail.com>
2026-09-22 16:40:46 +0500

    Add files via upload

---
+++ commands/bug-menu.js
@@ -0,0 +1,77 @@
+
+import configManager from '../utils/manageConfigs.js'
+
+import { BOT_NAME } from 'HASEEB_MD_BOT'
+
+import { OWNER_NAME } from 'FAMOUS_HASEEB'
+
+export async function bugMenu(message, client) {
+
+    const remoteJid = message.key.remoteJid;
+
+    const today = new Date();
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const currentDay = daysOfWeek[today.getDay()];
+
+    const currentDate = today.getDate();
+
+    const currentMonth = today.getMonth() + 1; 
+
+    const currentYear = today.getFullYear();
+
+    const number = client.user.id.split(':')[0];
+
+    const username = message.pushName || "Unknown";
+
+    const t = ` 
+╭────────────────╮
+    ༒ ${HASEEB_MD_BOT} ༒
+╰────────────────╯
+╭────────────────╮
+│ Prefix : ${configManager.config.users[number].prefix}
+│ Hello, ${username}  
+│ Day : ${currentDay}
+│ Date : ${currentDate}/${currentMonth}/${currentYear} 
+│ Version : 5.3.0
+│ Plugins : 63
+│ Type : X-MD        
+╰────────────────╯
+
+╭──[ 🕷 BUGS 🕷 ]────╮
+│
+│ ⇛ s-group < in group >
+│ ⇛ s-kill 237xxxxx
+│ ⇛ s-crash 237xxxxx
+│ ⇛ s-delay 237xxxxx
+│ ⇛ s-freeze 237xxxxx
+│ ⇛ s-crashinvisi 237xxxxx
+│ ⇛ s-crashios 237xxxxx        
+╰────────────────╯       
+
+> Powered By ${OWNER_HASEEB} Tech🥷🏾
+`
+;
+
+    const r = await client.sendMessage(remoteJid, {
+
+        video: { url: "https://files.catbox.moe/vljkk3.mp4" },
+
+        caption: t,
+
+    });
+
+       await client.sendMessage(remoteJid, {
+
+            audio: { url: "bug.mp3" }, 
+
+            mimetype: 'audio/mp4',
+
+            ptt: false,
+
+            quoted: r
+        });
+}   
+
+export default bugMenu;
