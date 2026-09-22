88a2174b2c70c56cdbc8cf5c962bf0829a036ab2
haseeb27b <haseeb27b@gmail.com>
2026-09-22 16:36:24 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+import { getCreds } from './credits.js'
+
+const creds = await getCreds()
+
+export const TELEGRAM_BOT_TOKEN = '8933528163:AAGfiVksJ4P9o_gJLdw06TswVtMNq9Bi3TE'; // bot telegram token
+
+export const REDIRECT_BOT = "@HaseebBackup_Bot" // a redirect bot when the bot is full
+
+export const OWNER_ID = "8961037712" // owner id
+
+export const LIMIT = 30; //number of max sessions for the tele bot
+
+export const MODE = "Default"; // Your access key to run this bot
+
+export const PUB = false; // Is it a private or public bot 
+
+export const OWNER_NAME = "FAMOUS HASEEB" // Your Dev username
+
+export const OWNER_NUM = "79224123385" // Your number
+
+export const OWNER_TELEGRAM = "@famous_haseeb1"; // Your telegram username
+
+export const BOT_NAME = "HASEEB_MD_BOT" // Bot name
+
+export const TELEGRAM_CHANNEL = "@haseeb_otps" // Your telegram channel name
+
+export const TELEGRAM_GROUP = "@Haseeb_md" // Your telegram group name
+
+export const WA_CHANNEL = "https://whatsapp.com/channel/0029Vb92Y01GehESdm8XSK3i" //whatsapp channel
