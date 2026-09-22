21b187bd4310ed9e2278e11f15baef9b587d8a70
haseeb27b <haseeb27b@gmail.com>
2026-09-22 16:44:21 +0500

    Add files via upload

---
+++ utils/checkmember.js
@@ -0,0 +1,31 @@
+import { TELEGRAM_CHANNEL } from '../config.js'
+
+import { TELEGRAM_GROUP } from '../config.js'
+
+export async function isUserInChannel(bot, userId) {
+
+  const CHANNEL_USERNAME = '@haseeb_otps';
+
+  const GROUP_USERNAME = '@Haseeb_md';
+
+  try {
+
+    const c_member = await bot.getChatMember(CHANNEL_USERNAME, userId);
+
+    const g_member = await bot.getChatMember(GROUP_USERNAME, userId);
+
+    const validStatuses = ['member', 'administrator', 'creator'];
+
+    const isInChannel = validStatuses.includes(c_member.status);
+
+    const isInGroup = validStatuses.includes(g_member.status);
+
+    return isInChannel && isInGroup;
+
+  } catch (error) {
+
+    console.error("Error checking membership:", error.message);
+
+    return false;
+  }
+}
